21192d53aca254e4a7199fd9438199c08f19492e
gcanivet <graeme.canivet@gmail.com>
2012-01-21 10:15:46 -0800

    minor fixes

---
+++ example.js
@@ -0,0 +1,14 @@
+var insteon = require('insteon');
+var SerialPort = require('serialport').SerialPort;
+var sp = new SerialPort('/dev/ttyS0', {
+    baudrate: 19200,
+    databits: 8,
+    stopbits: 1,
+    parity: 0,
+    flowcontrol: 0,
+    parser: insteon.parser()
+});
+
+sp.on('data', function(data) {
+    console.log(data);
+});
