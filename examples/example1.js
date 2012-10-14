2651d94063b875af99fa3741b44b59afa9e26e01
Matthew R Chase <matt@chasefox.net>
2012-10-14 10:01:14 -0400

    Improved example1

--- examples/example1.js
+++ examples/example1.js
@@ -1,9 +1,12 @@
 /*
-** Example 1: This example does not show usage of the node-insteon package.  This is an illustration
-**            of the serialport package with some.  It should output a list of ports available on
-**            the system, followed by an unending interval of "PLM getVersion"
+** Example 1: This example does not show usage of the node-insteon package except for the parser.
+**            The output should be a list of ports available on the system followed by a recurring
+**            PLM getVersion reply.
 */
-var SerialPort = require("serialport")
+
+//You may need to correct the paths in the requires to suit your environment.
+var insteon = require('insteon')        //necessary for insteon.parser()
+var SerialPort = require("serialport")  //everything else in this example.
 
 var listCallback = function(error, ports){
 	if(error){
@@ -24,7 +27,7 @@ SerialPort.list(listCallback)
 var cb = function(e, r){
 	console.log("Wrote to port.")
 	if(e) console.log("    received error: " + e)
-	//if(r) console.log("    received: " + r) //This is meaningless?  See the sp.on("data") below.
+	//if(r) console.log("    received: " r) //This is meaningless because it's partial?  See the sp.on("data") below.
 }
 var serialport = SerialPort.SerialPort
 var port = "/dev/ttyUSB0"  //change this to suit your system.
@@ -36,15 +39,17 @@ var sp = new serialport(port, {
     stopbits: 1,
     parity: 'none',
     flowcontrol: false,
-    parser: SerialPort.parsers.raw
+    //Graeme already provided a parser to buffer the data received on the serial port.  Only have one line
+    //below uncommented to see the difference.  Without buffering, sometimes the data received will be partial.
+    //parser: SerialPort.parsers.raw
+    parser: insteon.parser()
 })
 
-
-//This data must be buffered.  Run and you'll see sometimes the data is complete, but other times it is partial.
-//That it is not consistent means we need to collect it somehow before processing.
-sp.on("data", function (data) {
-	console.log("got data: "+ data.toString('hex'))
+sp.on('data', function(data) {
+    console.log("    Got data: " + data.toString('hex'))  //we don't need .toString('hex') when using insteon.parser()
+                                                          //but it makes output prettier when using raw parser.
 })
+
 //Two things here.  This is on setInterval to demonstrate the need for buffering.  But also, if we
 //just write to port immediatly after initialization, we'll receive error of port not connected yet.
 //I'm thinking there must be a callback feature for "on connect".  similarly to above, writes need buffering.
